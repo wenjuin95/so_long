16eff8c473d580055fd23461fb4ad60454006c7e
low <lowwenjuin27@gmail.com>
2024-02-18 20:21:39 +0800

    add some instruction for so_long.c

--- so_long2/src/so_long.c
+++ so_long2/src/so_long.c
@@ -12,31 +12,33 @@
 
 #include "so_long.h"
 
-int	main(int ac, char **av)
-{
-	t_data	game;
+/*
+ * 1. set all the struct to NULL and 0
+ * 2. read the .ber and assign to map
+ * 3. check if the map is valid & check if the input is valid
+ * 4. if all is valid, initialize the game
+ * 5. start the game
+ * 6. loop the game
+ * 7. if the map is not valid, free the map and print error
+ */
+int main(int ac, char **av) {
+  t_data game;
 
-	if (ac == 2)
-	{
-		ft_bzero(&game, sizeof(t_data));
-		game.map = read_map(av[1]);
-		if (game.map && check_map(&game) && check_input(av[1]))
-		{
-			init(&game);
-			gameplay(&game);
-			mlx_loop(game.mlx);
-		}
-		else
-		{
-			if (game.map)
-				free_all(game.map);
-			printf("Error\n");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		printf("Error\n");
-		exit(EXIT_FAILURE);
-	}
+  if (ac == 2) {
+    ft_bzero(&game, sizeof(t_data));
+    game.map = read_map(av[1]);
+    if (game.map && check_map(&game) && check_input(av[1])) {
+      init(&game);
+      gameplay(&game);
+      mlx_loop(game.mlx);
+    } else {
+      if (game.map)
+        free_all(game.map);
+      printf("Error\n");
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    printf("Error\n");
+    exit(EXIT_FAILURE);
+  }
 }
